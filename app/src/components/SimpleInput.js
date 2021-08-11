0f9d783f8325719287cbdb4217e92b2328cdfe62
Bernardo <bernardo.bt@gmail.com>
2021-08-11 14:21:57 -0300

    feature: implementação da validação do campo - onChange, onBlur, onSubmit

--- app/src/components/SimpleInput.js
+++ app/src/components/SimpleInput.js
@@ -1,15 +1,66 @@
+import { useState } from 'react';
+
+
 const SimpleInput = (props) => {
-  return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
-      </div>
-      <div className="form-actions">
-        <button>Submit</button>
-      </div>
-    </form>
-  );
+
+   const [enteredName, setEnteredName] = useState('');
+   const [enteredNameWasTouched, setEnteredNameWasTouched] = useState(false);
+   
+
+   const enteredNameIsValid = enteredName.trim() !== '';
+   const nameInputIsInvalid  = !enteredNameIsValid && enteredNameWasTouched;
+
+   let formIsValid = false;
+
+   if (enteredNameIsValid){
+      formIsValid = true;
+   }
+ 
+   const inputNameChangeHandler = event => {
+      setEnteredName(event.target.value);
+   }
+
+   const inputNameBlurHandler = event => {
+      setEnteredNameWasTouched(true)
+   }
+
+   const formSubmitHandler = event => {
+      event.preventDefault();
+
+      setEnteredNameWasTouched(true);
+
+      if (!enteredNameIsValid) {
+         return;
+      }
+
+      console.log(enteredName);
+
+      setEnteredName('');
+      setEnteredNameWasTouched(false);
+   }
+
+   const nameInputClasses = nameInputIsInvalid 
+      ? 'form-control invalid' 
+      : 'form-control';
+
+   return (
+      <form  onSubmit={formSubmitHandler}>
+         <div className={nameInputClasses}>
+            <label htmlFor='name'>Your Name</label>
+            <input 
+               type='text' 
+               id='name'
+               value={enteredName}
+               onChange={inputNameChangeHandler}  
+               onBlur={inputNameBlurHandler}              
+            />
+         </div>
+
+         <div className="form-actions">
+            <button disabled={!formIsValid} >Submit</button> 
+         </div>
+      </form>
+   );
 };
 
 export default SimpleInput;
